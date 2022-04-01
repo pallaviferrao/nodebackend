adeb092e5bbad55d92055db508f89a24418a896f
pallaviferrao <pallaviferrao2004@gmail.com>
2022-04-01 23:43:50 +0530

    port addtion

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 const { request } = require("http");
-const port = 4000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
