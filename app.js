89d056b20df85f1871be9df35ebb2964e68a3998
pallaviferrao <pallaviferrao2004@gmail.com>
2022-04-01 23:05:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const { request } = require("http");
+const port = 4000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post("/hello", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  console.log(req.body);
+  var username = req.body;
+  res.json({ a: username });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
