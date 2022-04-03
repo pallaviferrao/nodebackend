1a23a8dd8a0915caa87c0041c34ec257aea75e95
pallaviferrao <pallaviferrao2004@gmail.com>
2022-04-03 20:50:52 +0530

    update app

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 // const express = require("express");
 import { initializeApp } from "firebase/app";
 // import { getDatabase } from "firebase/database";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+// import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  query,
+  where,
+} from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -22,6 +31,7 @@ const firebaseConfig = {
   appId: process.env.API_ID,
   measurementId: process.env.MEASUREMENT_ID,
 };
+
 const apps = initializeApp(firebaseConfig);
 const db = getFirestore(apps);
 let uid = "";
@@ -58,6 +68,7 @@ app.post("/signup", (req, res) => {
       const user = userCredential.user;
       console.log(user);
       uid = user.uid;
+      res.json({ userId: uid, success: true });
       // ...
     })
     .catch((error) => {
@@ -65,8 +76,9 @@ app.post("/signup", (req, res) => {
       const errorMessage = error.message;
       console.log(errorCode);
       console.log(errorMessage);
+      res.json({ success: false });
     });
-  res.json({ b: true });
+  //   res.json({ b: true });
 });
 app.post("/login", (req, res) => {
   res.setHeader("Content-Type", "application/json");
@@ -82,16 +94,61 @@ app.post("/login", (req, res) => {
       console.log(user.uid);
       uid = user.uid;
       // ...
+      res.json({ userId: uid, success: true });
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       console.log(errorCode);
       console.log(errorMessage);
+      res.json({ success: false });
     });
-  res.json({ b: true });
 });
-app.post("/createGame");
+app.post("/createGame", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  let values = req.body[0];
+  console.log(values);
+  let userId = values.userId;
+  let gameName = values.gameName;
+  const addDatas = async (userId, gameName) => {
+    const newCityRef = collection(db, "usergames");
+    let data = {
+      name: "Pallasvi3",
+      gameName: gameName,
+      userId: userId,
+    };
+    let gamesId = await addDoc(newCityRef, data);
+
+    const gamesRef = collection(db, "Games");
+    let gameData = {
+      gameName: gameName,
+      userId: userId,
+      gameId: gamesId,
+    };
+    const gameId = await addDoc(gamesRef, gameData);
+    console.log(gameId.Id);
+  };
+  addDatas(userId, gameName);
+});
+app.post("/getGames", (req, res) => {
+  const getData = async () => {
+    const q = query(
+      collection(db, "usergames"),
+      where("userId", "==", req.body[0].userId)
+    );
+    // console.log(q);
+    const querySnapshot = await getDocs(q);
+    let games = [];
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      let x = [doc.id, doc.data().gameName];
+      games.push(x);
+    });
+    res.json({ success: true, games: games });
+  };
+  getData();
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
